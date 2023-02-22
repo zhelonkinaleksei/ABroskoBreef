54cdb7169f7453f604a9841a917d31530d8cfc2d
zhelonkinaleksei <120109097+zhelonkinaleksei@users.noreply.github.com>
2023-02-22 22:23:51 +0300

    1

--- src/JS/dropdown.js
+++ src/JS/dropdown.js
@@ -1,11 +1,3 @@
-if (window.NodeList && !NodeList.prototype.forEach) {
-	NodeList.prototype.forEach = function (callback, thisArg) {
-		thisArg = thisArg || window;
-		for (var i = 0; i < this.length; i++) {
-			callback.call(thisArg, this[i], i, this);
-		}
-	};
-}
 
 document.querySelectorAll('.custom__dropdown').forEach(function (dropDownWrapper) {
 
