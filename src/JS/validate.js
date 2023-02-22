6327f02d5ce919918ab03fd13d94ebc765b3e900
zhelonkinaleksei <120109097+zhelonkinaleksei@users.noreply.github.com>
2023-02-23 02:58:56 +0300

    1

--- src/JS/validate.js
+++ src/JS/validate.js
@@ -8,6 +8,10 @@ let form = document.querySelector('.breef__form'),
         let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(String(email).toLowerCase());
     }
+    function validatePhone(phone) {
+        let re = /^[0-9\s]*$/;
+        return re.test(String(phone));
+    }
 
     form.onsubmit = function () {
         let emailVal = inputEmail.value,
@@ -19,6 +23,7 @@ let form = document.querySelector('.breef__form'),
         formInputs.forEach(function (input) {
             if (input.value === '' ) {
                 input.classList.add('error');
+                
         
             } else {
                 input.classList.remove('error');
@@ -28,28 +33,58 @@ let form = document.querySelector('.breef__form'),
         formTextarea.forEach(function (textarea) {
             if (textarea.value === '' ) {
                 textarea.classList.add('error');
+                
         
             } else {
                 textarea.classList.remove('error');
+                
             }
         });
 
+        if (!validatePhone(phoneVal)) {
+            alert('Недопустимый телефон');
+            inputPhone.classList.add('error');
+            return false;
+        } else {
+            inputPhone.classList.remove('error');
+        }
         if (emptyInputs.length !== 0) {
-            console.log('inputs not filled');
+            alert('Заполните все поля');
             return false;
 
         }
         if(emptyTextarea.length !== 0) {
-            console.log('Textarea not filled');
+            alert('Заполните все поля');
             return false;
         }
         if(!validateEmail(emailVal)) {
-            console.log('email not valid');
+            alert('Недопустимый Email');
             inputEmail.classList.add('error');
             return false;
         } else {
             inputEmail.classList.remove('error');
         }
-    
+        
+      
  
     }
+
+    function fileValidation() {
+        let fileInput =
+            document.getElementById('breefattach');
+         
+        let filePath = fileInput.value;
+     
+        // Allowing file type
+        let allowedExtensions =
+/(\.jpg|\.txt|\.pdf|\.word|\.png)$/i;
+         
+        if (!allowedExtensions.exec(filePath)) {
+            alert('Недопустимый тип файла');
+            fileInput.value = '';
+            return false;
+        }
+    }
+
+
+    form.onsubmit
