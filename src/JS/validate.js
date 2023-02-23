447410a7a4047b2715b1c07e41e57b412ed41ba9
zhelonkinaleksei <120109097+zhelonkinaleksei@users.noreply.github.com>
2023-02-23 23:51:49 +0300

    1

--- src/JS/validate.js
+++ src/JS/validate.js
@@ -1,9 +1,13 @@
-let form = document.querySelector('.breef__form'),
+
+
+form.onsubmit = async function(e) {
+    e.preventDefault(); // Остановить стандартное поведение формы
+    let form = document.querySelector('.breef__form'),
     formInputs = document.querySelectorAll('.js-input'),
     formTextarea = document.querySelectorAll('.js-textarea'),
     inputEmail = document.querySelector('.js-input-email'),
     inputPhone = document.querySelector('.js-input-phone');
-
+   
     function validateEmail(email) {
         let re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
         return re.test(String(email).toLowerCase());
@@ -12,116 +16,136 @@ let form = document.querySelector('.breef__form'),
         let re = /^[0-9\s]*$/;
         return re.test(String(phone));
     }
+  
+    let emailVal = inputEmail.value,
+        phoneVal = inputPhone.value;
+    emptyInputs = Array.from(formInputs).filter(input => input.value === '');
+    emptyTextarea = Array.from(formTextarea).filter(textarea => textarea.value === '');
+  
+    formInputs.forEach(function(input) {
+      if (input.value === '') {
+        input.classList.add('error');
+      } else {
+        input.classList.remove('error');
+      }
+    });
+  
+    formTextarea.forEach(function(textarea) {
+      if (textarea.value === '') {
+        textarea.classList.add('error');
+      } else {
+        textarea.classList.remove('error');
+      }
+    });
+  
+    if (!validatePhone(phoneVal)) {
+      alert('Недопустимый телефон');
+      inputPhone.classList.add('error');
+      return false;
+    } else {
+      inputPhone.classList.remove('error');
+    }
+  
+    if (emptyInputs.length !== 0) {
+      alert('Заполните все поля');
+      return false;
+    }
+  
+    if (emptyTextarea.length !== 0) {
+      alert('Заполните все поля');
+      return false;
+    }
+  
+    if (!validateEmail(emailVal)) {
+      alert('Недопустимый Email');
+      inputEmail.classList.add('error');
+      return false;
+    } else {
+      inputEmail.classList.remove('error');
+    };
 
-    form.onsubmit = function () { 
-   
-        let emailVal = inputEmail.value,
-            phoneVal = inputPhone.value;
-            emptyInputs = Array.from(formInputs).filter(input => input.value === '');
-            emptyTextarea = Array.from(formTextarea).filter(textarea => textarea.value === '');
 
-
-        formInputs.forEach(function (input) {
-            if (input.value === '' ) {
-                input.classList.add('error');
-                
-        
-            } else {
-                input.classList.remove('error');
-            }
-        });
-
-        formTextarea.forEach(function (textarea) {
-            if (textarea.value === '' ) {
-                textarea.classList.add('error');
-                
-        
-            } else {
-                textarea.classList.remove('error');
-                
-            }
-        });
-
-        if (!validatePhone(phoneVal)) {
-            alert('Недопустимый телефон');
-            inputPhone.classList.add('error');
-            return false;
-        } else {
-            inputPhone.classList.remove('error');
+    // Если все проверки пройдены успешно, отправляем форму
+    let formData = new FormData(form);
+    let xhr = new XMLHttpRequest();
+  
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4) {
+        if (xhr.status === 200) {
+          console.log('Отправлено');
         }
-        if (emptyInputs.length !== 0) {
-            alert('Заполните все поля');
-            return false;
+      }
 
-        }
-        if(emptyTextarea.length !== 0) {
-            alert('Заполните все поля');
-            return false;
-        }
-        if(!validateEmail(emailVal)) {
-            alert('Недопустимый Email');
-            inputEmail.classList.add('error');
-            return false;
-        } else {
-            inputEmail.classList.remove('error');
-        }
-       
-   
-        
-        
+    }
  
-    }
-    
+  
+  
+    xhr.open('POST', 'formsend.php', true);
+    xhr.send(formData);
+  
+    form.reset();
+    alert('Ваша форма отправлена, мы скоро с вами свяжемся, спасибо!');
+  };
 
-    function fileValidation() {
-        let fileInput =
-            document.getElementById('breefattach');
-         
-        let filePath = fileInput.value;
+  function fileValidation() {
+    let fileInput =
+        document.getElementById('breefattach');
      
-        // Allowing file type
-        let allowedExtensions =
+    let filePath = fileInput.value;
+ 
+    // Allowing file type
+    let allowedExtensions =
 /(\.jpg|\.txt|\.pdf|\.word|\.png)$/i;
-         
-        if (!allowedExtensions.exec(filePath)) {
-            alert('Недопустимый тип файла');
-            fileInput.value = '';
-            return false;
-        }
-       
-       
-      
+     
+    if (!allowedExtensions.exec(filePath)) {
+        alert('Недопустимый тип файла');
+        fileInput.value = '';
+        return false;
     }
-
-
-    form.onsubmit = async (e) =>{
-        e.preventDefault(form)
-        let formData = new FormData(form);
-        let xhr = new XMLHttpRequest();
-
-            xhr.onreadystatechange = function() {
-                if (xhr.readyState === 5) {
-                    if (xhr.status === 200) {
-                        console.log('Отправлено');
-                    }
-                }
-            }
-
-            xhr.open('POST', 'formsend.php', true);
-            xhr.send(formData);
-
-            form.reset();
-            alert('Ваша форма отправлена, мы скоро с вами свяжемся, спасибо!');
-            
-            
-            
-    }
+   
+  
+  
+};
+  
 
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
     
 
     
